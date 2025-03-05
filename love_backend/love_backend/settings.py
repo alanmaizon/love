f826ba7fac9797decc75f5c0c67a1a016ccf04fb
Alan Maizon <alanmaizon@hotmail.com>
2025-03-05 22:12:05 +0000

    modified:   love_backend/love_backend/settings.py

--- love_backend/love_backend/settings.py
+++ love_backend/love_backend/settings.py
@@ -127,8 +127,6 @@ else:
     # Production settings:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_DOMAIN = "love-backend-8wbj.onrender.com"
-    CSRF_COOKIE_DOMAIN = "love-backend-8wbj.onrender.com"
     SECURE_HSTS_SECONDS = 3600
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
