d234514fb7865cd7c4e51416e2c1ac8bab800d73
Alan Maizon <58921943+alanmaizon@users.noreply.github.com>
2025-03-07 23:16:56 +0000

    Update admin.py

--- love_backend/donations/admin.py
+++ love_backend/donations/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
-from .models import Charity, Donation
+from django.utils.html import format_html
+from .models import Profile, Charity
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'display_profile_picture', 'bride_name', 'groom_name')
+    
+    def display_profile_picture(self, obj):
+        if obj.profile_picture:
+            return format_html('<img src="{}" style="max-height: 50px;"/>', obj.profile_picture.url)
+        return "No Image"
+    display_profile_picture.short_description = "Profile Picture"
 
 @admin.register(Charity)
 class CharityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'website')
-    search_fields = ('name',)
-
+    list_display = ('name', 'display_logo')
     
-@admin.register(Donation)
-class DonationAdmin(admin.ModelAdmin):
-    list_display = ('donor_name', 'donor_email', 'amount', 'status', 'created_at')
-    list_filter = ('status', 'created_at')
-    search_fields = ('donor_name', 'donor_email')
+    def display_logo(self, obj):
+        if obj.logo:
+            return format_html('<img src="{}" style="max-height: 50px;"/>', obj.logo.url)
+        return "No Logo"
+    display_logo.short_description = "Logo"
