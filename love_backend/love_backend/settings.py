61c4d52fb71e83140aefed7d4d4ae1740dfd686e
Alan Maizon <alanmaizon@hotmail.com>
2025-03-09 21:42:51 +0000

    Replace AWS S3 storage with Cloudinary for media handling and update requirements

--- love_backend/love_backend/settings.py
+++ love_backend/love_backend/settings.py
@@ -34,22 +34,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'donations',
-    'storages',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'lovethatgivesback-images'
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-west-1')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_DEFAULT_ACL = None
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+# Cloudinary settings
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_URL = 'https://res.cloudinary.com/dnkrfdjzl/'
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
