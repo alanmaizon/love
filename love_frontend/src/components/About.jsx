13ccb25c1cb47403e07f3c31d03e8c065d089f30
Alan Maizon <alanmaizon@hotmail.com>
2025-03-11 01:59:12 +0000

    add bio

--- love_frontend/src/components/About.jsx
+++ love_frontend/src/components/About.jsx
@@ -51,10 +51,17 @@ function Bio() {
 
       {/* Text Section */}
       <div style={{ flex: 1 }}>
-        <h1 style={{ color: '#EAD7BB' }}>About Us</h1>
+        <h1 style={{ color: '#EAD7BB' }}>The Greatest Gift is Your Love and Presence</h1>
         <p style={{ color: '#EAD7BB' }}>
           Welcome to our story. Here you'll learn about our journey, our love, and our dreams.
           We hope our story inspires you as much as your support inspires us.
+        </p>
+        <p style={{ color: '#EAD7BB' }}>
+        On April 26, 2025, we will be celebrating one of the most special days of our lives, and we feel incredibly grateful to share it with you. Your love, support, and presence at our wedding are the greatest gifts we could ever receive.
+        If you'd like to contribute in another way, we have created this space with meaningful options. Instead of traditional gifts, we want our union to be an opportunity to give back. That’s why we have chosen three charitable organizations that inspire us and reflect the values we share. Any donation, big or small, will be a beautiful gesture of love in honor of our wedding.
+        Thank you for being part of our journey and for helping us make love something that extends beyond our special day.
+        With love,
+        <em>Alan & Anna</em>
         </p>
       </div>
     </div>
