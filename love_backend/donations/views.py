60c0974952d25a1a28d3417d10b5fde910479d9b
Alan Maizon <alanmaizon@hotmail.com>
2025-03-14 07:55:00 +0000

    add helpers.py

--- love_backend/donations/views.py
+++ love_backend/donations/views.py
@@ -12,47 +12,7 @@ from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Sum, Count, F, ExpressionWrapper, DecimalField
 from decimal import Decimal
-from django.utils.crypto import get_random_string
-from django.shortcuts import render
-from django.core.mail import EmailMultiAlternatives
-from django.conf import settings
-from django.template.loader import render_to_string
-from django.templatetags.static import static
-from django.utils.html import strip_tags
-
-def send_donation_confirmation_email(donation):
-    """
-    Sends a stylish email confirmation using SVG images from static files.
-    """
-    subject = "🎉 Thank You for Your Gift!"
-    from_email = settings.DEFAULT_FROM_EMAIL
-    recipient_list = [donation.donor_email]
-
-    # Get URLs for SVG files
-    flower_top_url = settings.SITE_URL + static("images/flower_top.svg")
-    flower_bottom_url = settings.SITE_URL + static("images/flower_bottom.svg")
-
-    # Email context
-    context = {
-        "donor_name": donation.donor_name,
-        "amount": donation.amount,
-        "reference_id": f"GIFT-{donation.id}",
-        "charity_name": donation.charities.first().name if donation.charities.exists() else "All Charities",
-        "couple_allocation": donation.amount * 0.5,
-        "charity_allocation": donation.amount * 0.5,
-        "flower_top_url": flower_top_url,
-        "flower_bottom_url": flower_bottom_url,
-    }
-
-    # Render HTML email template
-    html_content = render_to_string("thank_you.html", context)
-    text_content = strip_tags(html_content)
-
-    # Create email message
-    email = EmailMultiAlternatives(subject, text_content, from_email, recipient_list)
-    email.attach_alternative(html_content, "text/html")
-
-    email.send()
+from .helpers import send_donation_confirmation_email 
 
 @api_view(['GET', 'PUT'])
 @authentication_classes([CsrfExemptSessionAuthentication])
@@ -146,9 +106,11 @@ class DonationViewSet(CsrfExemptMixin, viewsets.ModelViewSet):
         donation = self.get_object()
         donation.status = 'confirmed'
         donation.save()
+
+        send_donation_confirmation_email(donation)
+
         serializer = self.get_serializer(donation)
-        return Response(serializer.data, status=drf_status.HTTP_200_OK)
-    
+
     @action(detail=True, methods=['patch'], url_path='fail')
     def fail_donation(self, request, pk=None):
         donation = self.get_object()
