316ba91327a9f138ef09513c118ad0370c9fd360
Alan Maizon <alanmaizon@hotmail.com>
2025-03-14 08:41:08 +0000

    add dirs

--- love_backend/love_backend/settings.py
+++ love_backend/love_backend/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'love_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'love_backend', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
