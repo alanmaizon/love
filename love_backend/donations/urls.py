b1df3edf3830448550f2cbd10b6e0c605f98b06e
Alan Maizon <alanmaizon@hotmail.com>
2025-04-10 00:35:58 +0100

    Add missing YouTube video details path to urlpatterns

--- love_backend/donations/urls.py
+++ love_backend/donations/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import YouTubeProxyView, DonationViewSet, CharityViewSet, login_view, logout_view, donation_analytics, profile_detail, public_profile
 from .charts import combined_charts
+from . import views
 
 router = DefaultRouter()
 router.register(r'donations', DonationViewSet)
@@ -12,6 +13,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('youtube-video-details', views.youtube_video_details, name='youtube_video_details'),
     path('youtube-proxy/', YouTubeProxyView.as_view(), name='youtube-proxy'),
     path('analytics/', donation_analytics, name='donation-analytics'),
     path('charts/', combined_charts, name='charts'),
